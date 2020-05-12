3262907aeaadd41dcb81ae14ccce2bc56fced7f9
Romain Couperier <romain.couperier@mail.mcgill.ca>
2020-05-12 11:42:48 -0400

    Initial Commit

---
+++ fund_allocation.py
@@ -0,0 +1,29 @@
+import requests
+import pandas as pd
+from datetime import datetime 
+import json
+
+def FundAllocation():
+    sent = requests.get('https://api.ethplorer.io/getAddressInfo/0xCB60D600160D005845Ec999f64266D5608fd8943?apiKey=freekey').json()
+
+    data = {'Symbol' : ['ETH'], 'Balance' : [sent['ETH']['balance'] * sent['ETH']['price']['rate']]}
+    df = pd.DataFrame(data)
+
+    tokens = sent['tokens']
+    for token in tokens:
+        if token['tokenInfo']['price'] != False:
+            newRow = {'Symbol' : token['tokenInfo']['symbol'], 'Balance' : token['balance'] * token['tokenInfo']['price']['rate'] / 10 ** (int(token['tokenInfo']['decimals']))}
+            df = df.append(newRow, ignore_index=True)
+    df.to_csv('FundAllocation.csv', index=False)
+
+def main():
+    FundAllocation()
+
+    now = datetime.now()
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+    f = open("run_log.txt","a")
+    f.write("Ran at " + dt_string + "\n")
+    f.close() 
+
+if __name__ == '__main__':
+    main()
