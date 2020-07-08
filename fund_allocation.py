a312ff202f435f11591ed1f5124c4f2a25cbc820
RomCoup <35381665+RomCoup@users.noreply.github.com>
2020-07-08 10:42:56 -0400

    Update fund_allocation.py

--- fund_allocation.py
+++ fund_allocation.py
@@ -15,9 +15,23 @@ def FundAllocation():
             newRow = {'Symbol' : token['tokenInfo']['symbol'], 'Balance' : token['balance'] * token['tokenInfo']['price']['rate'] / 10 ** (int(token['tokenInfo']['decimals']))}
             df = df.append(newRow, ignore_index=True)
     df.to_csv('FundAllocation.csv', index=False)
+    
+def FundAllocationInsula():
+    sent = requests.get('https://api.ethplorer.io/getAddressInfo/0xCB60D600160D005845Ec999f64266D5608fd8943?apiKey=freekey').json()
+
+    data = {'Symbol' : ['ETH'], 'Balance' : [sent['ETH']['balance'] * sent['ETH']['price']['rate']]}
+    df = pd.DataFrame(data)
+
+    tokens = sent['tokens']
+    for token in tokens:
+        if token['tokenInfo']['price'] != False:
+            newRow = {'Symbol' : token['tokenInfo']['symbol'], 'Balance' : token['balance'] * token['tokenInfo']['price']['rate'] / 10 ** (int(token['tokenInfo']['decimals']))}
+            df = df.append(newRow, ignore_index=True)
+    df.to_csv('FundAllocationInusla.csv', index=False)
 
 def main():
     FundAllocation()
+    FundAllocationInsula()
 
     now = datetime.now()
     dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
