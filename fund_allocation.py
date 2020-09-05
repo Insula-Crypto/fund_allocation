739676abf3d28c30b836d2b50ee73337155f723a
RomCoup <35381665+RomCoup@users.noreply.github.com>
2020-09-05 16:10:06 +0200

    Update fund_allocation.py

--- fund_allocation.py
+++ fund_allocation.py
@@ -3,7 +3,7 @@ import pandas as pd
 from datetime import datetime 
 import json
 
-def FundAllocation():
+def AnastasiaAllocation():
     sent = requests.get('https://api.ethplorer.io/getAddressInfo/0x9C49c053a8b9106024793516EE3c5562875A5C9a?apiKey=freekey').json()
 
     data = {'Symbol' : ['ETH'], 'Balance' : [sent['ETH']['balance'] * sent['ETH']['price']['rate']]}
@@ -14,7 +14,21 @@ def FundAllocation():
         if token['tokenInfo']['price'] != False:
             newRow = {'Symbol' : token['tokenInfo']['symbol'], 'Balance' : token['balance'] * token['tokenInfo']['price']['rate'] / 10 ** (int(token['tokenInfo']['decimals']))}
             df = df.append(newRow, ignore_index=True)
-    df.to_csv('FundAllocation.csv', index=False)
+    df.to_csv('AnastasiaAllocation.csv', index=False)
+    
+
+def MaltaAllocation():
+    sent = requests.get('https://api.ethplorer.io/getAddressInfo/0x0d9596Afc608B3322A17118A573750045F52C0B8?apiKey=freekey').json()
+
+    data = {'Symbol' : ['ETH'], 'Balance' : [sent['ETH']['balance'] * sent['ETH']['price']['rate']]}
+    df = pd.DataFrame(data)
+
+    tokens = sent['tokens']
+    for token in tokens:
+        if token['tokenInfo']['price'] != False:
+            newRow = {'Symbol' : token['tokenInfo']['symbol'], 'Balance' : token['balance'] * token['tokenInfo']['price']['rate'] / 10 ** (int(token['tokenInfo']['decimals']))}
+            df = df.append(newRow, ignore_index=True)
+    df.to_csv('MaltaAllocation.csv', index=False)
     
 def FundAllocationInsula():
     sent = requests.get('https://api.ethplorer.io/getAddressInfo/0xCB60D600160D005845Ec999f64266D5608fd8943?apiKey=freekey').json()
@@ -30,8 +44,9 @@ def FundAllocationInsula():
     df.to_csv('FundAllocationInusla.csv', index=False)
 
 def main():
-    FundAllocation()
-    FundAllocationInsula()
+    AnastasiaAllocation()
+    MaltaAllocation()
+    ## FundAllocationInsula()
 
     now = datetime.now()
     dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
