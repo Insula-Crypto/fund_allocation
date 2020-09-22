b8b876459198d14103d6841892ec66410cbd5080
Romain Couperier <rcouperier@gmail.com>
2020-09-22 14:15:57 +0200

    Malta Allocation

--- fund_allocation.py
+++ fund_allocation.py
@@ -18,17 +18,33 @@ def AnastasiaAllocation():
     
 
 def MaltaAllocation():
-    sent = requests.get('https://api.ethplorer.io/getAddressInfo/0x0d9596Afc608B3322A17118A573750045F52C0B8?apiKey=freekey').json()
+    jsonData = '{"query": "{fundHoldings(where: {fund: \\"0x26491fc7da30b35d818de45982fb1de4f65ed8f5\\"}){asset {id}, assetGav}}"}'
+    data = requests.post('https://api.thegraph.com/subgraphs/name/melonproject/melon', data = jsonData).json()['data']
 
-    data = {'Symbol' : ['ETH'], 'Balance' : [sent['ETH']['balance'] * sent['ETH']['price']['rate']]}
-    df = pd.DataFrame(data)
+    assetDict = {
+        '0x0d8775f648430679a709e98d2b0cb6250d2887ef': 'BAT',
+        '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984': 'UNI',
+        '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599': 'WBTC',
+        '0x408e41876cccdc0f92210600ef50372656052a38': 'REN',
+        '0x514910771af9ca656af840dff83e8264ecf986ca': 'LINK',
+        '0x607f4c5bb672230e8672085532f7e901544a7375': 'RLC',
+        '0x960b236a07cf122663c4303350609a66a7b288c0': 'ANT',
+        '0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2': 'MKR',
+        '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2': 'ETH',
+        '0xe41d2489571d322189246dafa5ebde1f4699f498': 'ZRX',
+        '0xec67005c4e498ec7f55e092bd1d35cbc47c91892': 'MLN'
+    }
 
-    tokens = sent['tokens']
-    for token in tokens:
-        if token['tokenInfo']['price'] != False:
-            newRow = {'Symbol' : token['tokenInfo']['symbol'], 'Balance' : token['balance'] * token['tokenInfo']['price']['rate'] / 10 ** (int(token['tokenInfo']['decimals']))}
-            df = df.append(newRow, ignore_index=True)
-    df.to_csv('MaltaAllocation.csv', index=False)
+    assets = []
+    nav = []
+
+    for i in range(len(data['fundHoldings'])):
+        assets.append(assetDict[data['fundHoldings'][i]['asset']['id']])
+        nav.append(int(data['fundHoldings'][i]['assetGav']) / 1e18)
+
+    df = pd.DataFrame({'Value': nav}, index=assets)
+    df.to_csv('MaltaAllocation.csv')
+
     
 def FundAllocationInsula():
     sent = requests.get('https://api.ethplorer.io/getAddressInfo/0xCB60D600160D005845Ec999f64266D5608fd8943?apiKey=freekey').json()
